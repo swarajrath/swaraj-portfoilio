35d47d6dbbe30ac4a37726c931ab26eb7a3fab47
swarajrath <gulal009@gmail.com>
2021-01-22 14:05:31 +0100

    Description Change

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
                 <ul className="nav__links">
                     <li><a href='/skills'>Skills</a></li>
                     <li><a href='/projects'>Projects</a></li>
-                    <li><a href='/achievements'>Acheivement</a></li>
+                    <li><a href='/achievements'>Acheivements</a></li>
                     <li><a href='/experience'>Experience</a></li>
                 </ul>
             </nav>
